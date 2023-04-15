76189fcc3a7e1e980684391ffc526ac71b7ddb67
NikitaSazaev <130746443+NikitaSazaev@users.noreply.github.com>
2023-04-15 20:55:29 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal(".plus1", {scale: 1.2, interval: 200, duration: 1500,});
+ScrollReveal().reveal(".plus2", {scale: 1.2, interval: 200, duration: 1500,});
+ScrollReveal().reveal(".plus3", {scale: 1.2, interval: 200, duration: 1500,});
+
+ScrollReveal().reveal(".item", {scale: 1.05, duration: 1500, interval: 200});
+ScrollReveal().reveal(".item", {reset: true});
